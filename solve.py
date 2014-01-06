b2065c9bf5bf4f8437a6a9e0c80c9d691e86379c
Richard Stebbing <richie.stebbing@gmail.com>
2014-01-06 18:25:56 +1300

    Remove `main_test_shard_gradient` and `shard_gradient` (unused) from solve.py

--- solve.py
+++ solve.py
@@ -27,21 +27,6 @@ def subaxes(n):
 
     return f, axs
 
-# shard_gradient
-def shard_gradient(I, J, alpha, X, y, k, epsilon=1e-6):
-    shape = I.shape[:2]
-    domain = shape[::-1]
-    shard = Shard(X, k)
-
-    H, dX = shard(domain, return_dX=True, epsilon=epsilon)
-
-    d = alpha * (J - y)
-    R = (I - J) + d * H[..., np.newaxis]
-    J = dX[..., np.newaxis] * d
-    J = J.reshape(X.size, -1).transpose()
-    dX = np.dot(R.ravel(), J)
-    return dX.reshape(X.shape)
-
 # fit_shard
 def fit_shard(I, J, alpha, X, y, k, epsilon=1e-6, update_colours=False,
               limit_colours=True, check_gradients=False, **kwargs):
@@ -135,47 +120,3 @@ def colour_shard(I, J, alpha, X, k, limit_colours=True):
         y[y < 0.0] = 0.0
 
     return y
-
-# main_test_shard_gradient
-def main_test_shard_gradient():
-    INPUT_PATH = 'data/180,180-40,40,90,127,140,40-1,0,0-2.png'
-    I = plt.imread(INPUT_PATH).astype(np.float64)
-    shape = I.shape[:2]
-    domain = shape[::-1]
-
-    X = np.r_[40.0, 50, 100, 127, 140, 40].reshape(-1, 2)
-    y = np.r_[1.0, 0.0, 0.0]
-    k = 1.0
-    alpha = 0.5
-
-    # `J` is the initial image which the shard is blended with
-    J = np.zeros_like(I)
-    dX = shard_gradient(I, J, alpha, X, y, k)
-
-    def apply_gradient(eta):
-        X_ = X + eta * dX
-        shard = Shard(X_, k)
-
-        H = shard(domain)
-        d = alpha * (J - y)
-        R = (I - J) + d * H[..., np.newaxis]
-        r = R.ravel()
-        E = np.dot(r, r)
-        print '%.7g -> %.7g' % (eta, E)
-        return H
-
-    etas = np.arange(20) * -0.05
-    f, axs = subaxes(etas.shape[0])
-
-    for i, eta in enumerate(etas):
-        H = apply_gradient(eta)
-        Ji = (J * (1.0 - alpha * H[..., np.newaxis]) 
-              + alpha * y * H[..., np.newaxis])
-        axs[i].imshow(Ji)
-        axs[i].set_xticks([])
-        axs[i].set_yticks([])
-    plt.show()
-
-if __name__ == '__main__':
-    main_test_shard_gradient()
-    
