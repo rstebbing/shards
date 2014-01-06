e8984c8b1cdf3d7fbaad1382c9530b2a84f2a97a
Richard Stebbing <richie.stebbing@gmail.com>
2014-01-06 19:11:51 +1300

    Remove jobs/

--- jobs/firefox.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export PYTHONPATH=/users/RichardStebbing/Code/Python:$PYTHONPATH
-export LD_LIBRARY_PATH=/users/RichardStebbing/lib/InsightToolkit:/users/RichardStebbing/lib:/users/RichardStebbing/lib64
-cd /users/RichardStebbing/Code/Projects/transparency
-/users/RichardStebbing/bin/python solve_multiple_shards.py examples/firefox_100x100.png firefox_100x100_white_4.0_0.3_1000_10_update-colours white 4.0 0.3 1000 --maxiter 10 --update-colours
--- jobs/lichtenstein_150x111.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export PYTHONPATH=/users/RichardStebbing/Code/Python:$PYTHONPATH
-export LD_LIBRARY_PATH=/users/RichardStebbing/lib/InsightToolkit:/users/RichardStebbing/lib:/users/RichardStebbing/lib64
-cd /users/RichardStebbing/Code/Projects/transparency
-/users/RichardStebbing/bin/python solve_multiple_shards.py examples/lichtenstein_150x111.png lichtenstein_150x111_white_4.0_0.3_1000_10_update-colours white 4.0 0.3 1000 --maxiter 10 --update-colours
